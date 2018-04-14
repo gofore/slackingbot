1b201ac1c0a32d1e98606f1cb110b4f278eba94a
Aki Ristkari <aki@ristkari.fi>
2018-04-14 13:44:26 +0300

    slack event

--- api.py
+++ api.py
@@ -1,4 +1,10 @@
 import json
+import os
+import logging
+import urllib
+
+print('Loading function')
+# BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 
 def send_slack_message(event, context):
@@ -26,6 +32,24 @@ def send_slack_message(event, context):
 
 def get_historical_messages(event, context):
 
+    if "challenge" in event['body']:
+        print("Hellurei")
+        return event["challenge"]
+
+    logging.warn(json.dumps(event))
+    body = {
+        "hello": "world"
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+
+def slack_event_handler(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
